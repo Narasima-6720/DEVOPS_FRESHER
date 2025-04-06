fcc6b474e6b12a9395bfdc6e8c9a207dd41f1b30
Madanapalli Narasimha Reddy <159816623+Narasima-6720@users.noreply.github.com>
2025-04-06 21:25:20 +0530

    Create Jenkins_setup.sh

---
+++ Shell_Scripting/Jenkins_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#########################
+# Author: Narasimha Reddy
+# Date : 6/4/25
+# Description: This script will install jenkins and return password
+# Version : v1
+#########################
+
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install java-17-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo mount -o remount,size=2G /tmp/
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
