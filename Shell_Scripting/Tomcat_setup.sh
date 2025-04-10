18c210b3f600501054cc83ec914b72ebd2e40d6c
Madanapalli Narasimha Reddy <159816623+Narasima-6720@users.noreply.github.com>
2025-04-10 08:16:06 +0530

    Update Tomcat_setup.sh

--- Shell_Scripting/Tomcat_setup.sh
+++ Shell_Scripting/Tomcat_setup.sh
@@ -4,27 +4,20 @@
 #Description: This script is used to setup Tomcat
 #version: v1
 ##########################
-sudo yum install java -y
-cd /opt/
+
+
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.102/bin/apache-tomcat-9.0.102.tar.gz
-sudo tar -xvf apache-tomcat-9.0.102.tar.gz
-sudo rm -rf apache-tomcat-9.0.102.tar.gz
-cd /opt/apache-tomcat-9.0.102/webapps/manager/META_INF
-sudo sed -i 's/127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1"/".*/g' context.xml
+sudo tar -xvf /opt/apache-tomcat-9.0.102.tar.gz
+cd /opt/apache-tomcat-9.0.102/webapps/manager/META-INF
+sudo sed -i 's/"127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1"/".*"/g' context.xml
 cd /opt/apache-tomcat-9.0.102/conf
-sudo mv tomcat-users.xml tomcat-users-bkp-2025.xml
+sudo mv tomcat-users.xml tomcat-users_bkup_18mar2025.xml
 sudo echo '<?xml version="1.0" encoding="utf-8"?>
-      <tomcat-users>
-      <role rolename="manager-gui"/>
-      <user username="tomcat" password="tomcat" roles="manager-gui, manager-script, manager-status"/>
-      </tomcat-users>' > tomcat-users.xml
-cd  apache-tomcat-9.0.102/conf/
-sudo sed -i 's/Connector port="8080"/Connector port="8081"/g' server.xml
+        <tomcat-users>
+        <role rolename="manager-gui"/>
+        <user username="tomcat" password="tomcat" roles="manager-gui, manager-script, manager-status"/>
+        </tomcat-users>' > tomcat-users.xml
+cd /opt/apache-tomcat-9.0.102/conf/
+sudo sed -i 's/Connector port="8080"/Connector port="8090"/g' server.xml
 sudo /opt/apache-tomcat-9.0.102/bin/startup.sh
-
-
-
-
-
-
 
